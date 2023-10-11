dafc85cbb58e8e1517ad09e489b3099a8628c172
harsh <dhoneharshal@gmail.com>
2023-10-11 20:20:17 +0530

    Injection of properties

--- src/main/java/com/example/demo/rest/FirstfunController.java
+++ src/main/java/com/example/demo/rest/FirstfunController.java
@@ -1,13 +1,17 @@
 package com.example.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstfunController {
 
-    @GetMapping("/")
+
+    @Value("${coach.name}") private String coachName;
+    @Value("${team.name}") private String teamName;
+    @GetMapping(value = "/")
     public String sayHello(){
-        return "Hello There...! us is spring Boot app";
+        return "Hello There...! coach "+coachName+" from "+teamName;
     }
 }
